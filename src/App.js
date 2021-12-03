6fa311fdf327f0ab4457feb11dbb901387f46827
musta10 <mustafaenfadal@gmail.com>
2021-12-03 21:05:30 +0100

    navegation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header"
 import {BrowserRouter} from 'react-router-dom'
 
+
 function App() {
   return (
     <BrowserRouter>
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./css/header.css";
 import { FaInfinity, FaWallet } from "react-icons/fa";
 
@@ -8,31 +8,31 @@ function Header() {
     <div className="App-Header">
       <div className="header_Left">
         <FaInfinity />
-        <p>ethernity</p>
+        <p className="separ">ethernity</p>
       </div>
       <nav className="header_Right">
-        <ul>
+        <ul className="header_List">
           <li>
-            <Link to="/">Marketplace</Link>
+            <NavLink activeClassName="active" to="/">Marketplace</NavLink>
           </li>
           <li>
-           <Link to="/">Get ERN</Link>
+           <NavLink activeClassName="active"  to="/">Get ERN</NavLink>
           </li>
           <li>
-            <Link to="/">Polygon Bridge</Link>
+            <NavLink activeClassName="active"  to="/">Polygon Bridge</NavLink>
           </li>
           <li>
-            <Link to="/">Farm Stones</Link>
+            <NavLink activeClassName="active"  to="/">Farm Stones</NavLink>
           </li>
           <li>
-            <Link to="/">Staking</Link>
+            <NavLink activeClassName="active"  to="/">Staking</NavLink>
           </li>
-          <FaWallet />
+          <FaWallet className="icon-color" />
           <li>
-            <Link to="/">Sign-In</Link>
+            <NavLink activeClassName="active"  to="/">Sign-In</NavLink>
           </li>
           <li>
-            <Link to="/">Sign-Up</Link>
+            <NavLink  activeClassName="active" to="/">Sign-Up</NavLink>
           </li>
         </ul>
       </nav>
