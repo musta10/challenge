c2298c161ad61cf66fe57b6a04dc06f6c1df2139
musta10 <mustafaenfadal@gmail.com>
2021-12-03 13:03:13 +0100

    header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import "./css/header.css"
+import React from "react";
+import "./css/header.css";
+import { FaInfinity, FaWallet } from "react-icons/fa";
 
 function Header() {
-    return (
-        <div className="App-header">
-            
-        </div>
-    )
+  return (
+    <div className="App-Header">
+      <div className="header_Left">
+        <FaInfinity />
+        <p>ethernity</p>
+      </div>
+      <navbar className="header_Right">
+        <ul>
+            <li><a href="#">Marketplace</a></li>
+            <li><a href="#">Get ERN</a></li>
+            <li><a href="#">Polygon Bridge</a></li>
+            <li><a href="#">Farm Stones</a></li>
+            <li><a href="#">Staking</a></li>
+            <FaWallet/>
+            <li><a href="#">Sign-In</a></li>
+            <li><a href="#">Sign-Up</a></li>
+        </ul>
+      </navbar>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
